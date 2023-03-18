ef9e5571ecfc90e8cc563009c7e6544d39cadeea
Isla <isla@Islas-MacBook-Pro.local>
2023-03-17 20:19:14 -0400

    Finished Hybrid 3

--- js/main.js
+++ js/main.js
@@ -3,32 +3,35 @@ import MEDIA from './data.js';
 
 const APP = {
   audio: new Audio(),
-  src: MEDIA,
   currentTrack: 0,
+  audioFolder: 'media/',
+  thumbnailFolder: 'img/',
+  largeImgFolder: 'img/',
 
   init: () => {
     APP.addListeners();
     APP.buildPlaylist();
     APP.loadCurrentTrack();
-    APP.errorHandler();
    },
   addListeners: () => {
+    document.getElementById('playlist').addEventListener('click', APP.clickPlaylist);
     document.getElementById('btnPrev').addEventListener('click', APP.playPrevious);
     document.getElementById('btnPlay').addEventListener('click', APP.togglePlayPause);
     document.getElementById('btnNext').addEventListener('click', APP.playNext);
-    APP.audio.addEventListener('error', APP.errorHandler);
     APP.audio.addEventListener('play', APP.updatePlaylist);
     APP.audio.addEventListener('pause', APP.updatePlaylist);
     APP.audio.addEventListener('ended', APP.playNext);
+    APP.audio.addEventListener('timeupdate', APP.updateTime);
+    APP.audio.addEventListener('durationchange', APP.updateDuration);
+
   },
   buildPlaylist: () => {
-    //read the contents of MEDIA and create the playlist
     let ul = document.getElementById('playlist');
     let html = MEDIA.map((item, id) => {
       return `
-          <li class="track__item" id=${id}>
+          <li class="track__item" data-filename="${item.track}" data-src="${APP.audioFolder}${item.track}">
             <div class="track__thumb">
-              <img src="${item.thumbnail}" alt="Album Art for ${item.title}">
+              <img src="${APP.thumbnailFolder}${item.thumbnail}" alt="Album Art for ${item.title}">
             </div>
             <div class="track__details">
               <p class="track__title">${item.title}</p>
@@ -41,23 +44,33 @@ const APP = {
         `;
       }).join('');
       ul.innerHTML = html;
-    
-    document.querySelectorAll('.track__item').forEach(track => {
-      track.addEventListener('click', () => {
-        const largeImg = document.querySelector('.album_art__full img');
-        largeImg.src = `${MEDIA[track.id].large}`;
-        APP.currentTrack = track.id;
-        APP.loadCurrentTrack();
-        APP.updatePlaylist();
-      });
-    });
   },
   updatePlaylist: () => {
     
   },
   loadCurrentTrack: () => {
-    APP.audio.src = `${APP.src[APP.currentTrack].src}`;
+    APP.audio.src = `./${APP.audioFolder}${MEDIA[APP.currentTrack].track}`;
+  }, 
+  clickPlaylist: (e) => {
+      let trackItem = e.target;
+      while (!trackItem.classList.contains('track__item')) 
+      { trackItem = trackItem.parentElement;
+      if (!trackItem) {
+      return;
+        }
+      }
+      if (trackItem) {
+      const largeImg = document.querySelector('.album_art__full img');
+      const src = trackItem.dataset.src;
+      const filename = trackItem.dataset.filename;
+      const id = MEDIA.findIndex(item => item.track === filename);
+      largeImg.src = `${APP.largeImgFolder}${MEDIA[id].large}`;
+      APP.currentTrack = id;
+      APP.loadCurrentTrack();
+      APP.updatePlaylist();
+    }
   },
+  
   togglePlayPause: () => {
     if (APP.audio.paused) {
       APP.audio.play();
@@ -67,27 +80,47 @@ const APP = {
       btnPlay.innerHTML= '<i class="material-icons">play_arrow</i>';
     }
   },
+  updateAlbumArt: () => {
+    const largeImg = document.querySelector('.album_art__full img');
+    largeImg.src = `${APP.largeImgFolder}${MEDIA[APP.currentTrack].large}`;
+  },
   playPrevious: () => {
-    //start the track loaded into APP.audio playing
+    APP.audio.pause();
+    APP.currentTrack--;
+    if (APP.currentTrack < 0) {
+        APP.currentTrack = MEDIA.length - 1;
+    }
+    APP.loadCurrentTrack();
+    APP.updateAlbumArt();
+    APP.audio.play();
   },
   playNext: () => {
-    //start the track loaded into APP.audio playing
+    APP.audio.pause();
+    APP.currentTrack++;
+    if (APP.currentTrack >= MEDIA.length) {
+        APP.currentTrack = 0;
+    }
+    APP.loadCurrentTrack();
+    APP.updateAlbumArt();
+    APP.audio.play();
   },
   pause: () => {
     APP.audio.pause();
   },
-  errorHandler: (ev) => {
-    if (!ev) return;
-    console.error("There was an error loading the audio:", ev);
+  updateTime: () => {
+    const displayTime = document.getElementById('current-time');
+    const currentTime = APP.audio.currentTime;
+    displayTime.innerText = `${APP.convertSeconds(currentTime)} |`;
   },
-  timeUpdate: () => {
-    const currentTimeDisplay = document.getElementById('current-time');
-    audio.addEventListener('timeupdate', () => {
-    const currentTime = Math.floor(audio.currentTime);
-    const minutes = Math.floor(currentTime / 60);
-    const seconds = Math.floor(currentTime % 60);
-    currentTimeDisplay.innerHTML = `${minutes}:${seconds} |`;
-  });
+  updateDuration: () => {
+  const durationDisplay = document.getElementById('duration');
+  const duration = APP.audio.duration;
+  durationDisplay.innerText = APP.convertSeconds(duration);
+  },
+  convertSeconds: (seconds) => {
+    const minutes = Math.floor(seconds / 60);
+    const secondsLeft = Math.floor(seconds % 60);
+    return `${minutes}:${secondsLeft < 10 ? '0' : ''}${secondsLeft}`;
   },
 };
 
